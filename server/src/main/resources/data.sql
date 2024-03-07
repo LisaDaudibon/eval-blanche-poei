dbd319ad31764d59b876515af45abd099a5e1610
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-03-07 15:35:20 +0100

    :card_file_box: creating the first part of the schema and insert a few data

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO game (word_to_guess, description) VALUES
+    ( 'lapin', 'Description for lapin'),
+    ( 'potate', 'Description for potate');
+
+-- INSERT INTO round (attempt, state, game_uuid) VALUES
+--     ('0', 'ONGOING', '93a6a0a8-25b5-4c35-92b4-4a2baa1f96cd');
