8ba8502de52c95804a7dfa7efc60143f6a57833b
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-03-11 12:50:14 +0100

    :art: moving the fetch all to the home component

---
+++ client/src/app/components/azerty-keyboard/azerty-keyboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-azerty-keyboard',
+  templateUrl: './azerty-keyboard.component.html',
+  styleUrl: './azerty-keyboard.component.css'
+})
+export class AzertyKeyboardComponent {
+
+}
--- client/src/app/views/game-board/game-board.component.ts
+++ client/src/app/views/game-board/game-board.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Game } from '../../models/game';
-import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-game-board',
@@ -9,15 +6,5 @@ import { GameService } from '../../services/game.service';
   styleUrl: './game-board.component.css'
 })
 export class GameBoardComponent {
-  $fetchedGames: Observable<Game[]>;
-  $fetchedGame: Observable<Game>;
 
-  constructor( private _gameService: GameService) { 
-    this.$fetchedGames = this._gameService.$fetchGames
-    this.$fetchedGame = this._gameService.fetchGameById('e40f7f5b-beda-4025-97d8-d047f5806083  ')
-  }
-
-  trackById ( index: number, game: Game): string {
-    return game.id;
-  }
 }
