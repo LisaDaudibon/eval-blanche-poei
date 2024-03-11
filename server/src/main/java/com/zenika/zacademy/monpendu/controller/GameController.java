fe265ba544921ff0a18258db9e65c77e1835056f
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-03-11 16:03:13 +0100

    📝 adding wagger infos

--- server/src/main/java/com/zenika/zacademy/monpendu/controller/GameController.java
+++ server/src/main/java/com/zenika/zacademy/monpendu/controller/GameController.java
@@ -4,6 +4,11 @@ import com.zenika.zacademy.monpendu.controller.dto.gameDto.GameDtoOut;
 import com.zenika.zacademy.monpendu.controller.dto.gameDto.GameMapper;
 import com.zenika.zacademy.monpendu.service.GameService;
 import com.zenika.zacademy.monpendu.service.exception.NotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +29,10 @@ public class GameController {
     private final Logger logger = LoggerFactory.getLogger(GameController.class);
 
     @GetMapping
+    @Operation(summary = "Get all games")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The games found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = GameDtoOut[].class))})
+    })
     @ResponseStatus(HttpStatus.OK)
     public List<GameDtoOut> findAll() {
         logger.info("Show a list of all games");
@@ -33,6 +42,12 @@ public class GameController {
     }
 
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get a game from its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The game found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = GameDtoOut.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Game not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public GameDtoOut findById(@PathVariable UUID id) throws NotFoundException {
         logger.info("Show the game with id ${id}");
@@ -40,6 +55,11 @@ public class GameController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new round")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The round created", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = GameDtoOut.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid round object supplied", content = @Content),
+    })
     @PostMapping("/{id}/rounds")
     public UUID create( @PathVariable UUID id ) throws NotFoundException {
         logger.info("Create a new round");
--- server/src/main/java/com/zenika/zacademy/monpendu/controller/RoundController.java
+++ server/src/main/java/com/zenika/zacademy/monpendu/controller/RoundController.java
@@ -5,6 +5,10 @@ import com.zenika.zacademy.monpendu.controller.dto.roundDto.RoundMapper;
 import com.zenika.zacademy.monpendu.service.RoundService;
 import com.zenika.zacademy.monpendu.service.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +25,14 @@ import java.util.UUID;
 public class RoundController {
     private final Logger logger = LoggerFactory.getLogger(RoundController.class);
 
-
     private final RoundService roundService;
     private final RoundMapper roundMapper;
 
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get all rounds ever created")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The rounds found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RoundDtoOut[].class))})
+    })
     @GetMapping
     public List<RoundDtoOut> findAll() {
         return this.roundService.findAll().stream()
@@ -34,6 +41,12 @@ public class RoundController {
     }
 
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get a round from its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The round found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RoundDtoOut.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Round not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public RoundDtoOut findById(@PathVariable UUID id ) throws NotFoundException {
         return this.roundMapper.toDto(this.roundService.findOneById(id));
@@ -41,6 +54,10 @@ public class RoundController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Play a letter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The letter is added to the list of letters", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RoundDtoOut.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid letter object supplied", content = @Content),
+    })
     @PostMapping("/{id}/searched/{c}")
     public RoundDtoOut updateLetterGuessed ( @PathVariable UUID id, @PathVariable("c") String letterGuessed ) throws NotFoundException {
         logger.info("Play a letter");
