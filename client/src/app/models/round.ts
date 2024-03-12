8b95c10610fb96baaeb68b347c39cfbc41bce859
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-03-12 13:44:27 +0100

    :art: (client) adding description to rounds

--- client/src/app/models/round.ts
+++ client/src/app/models/round.ts
@@ -1,3 +1,3 @@
 export class Round {
-  constructor(public id: string, public attempt: number, public lettersSearched: string[], public state: string, public roundWord: string) {}
+  constructor(public id: string, public attempt: number, public lettersSearched: string[], public state: string, public roundWord: string, public gameDescription: string) {}
 }
